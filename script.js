879c29ae44aaff38458d0030dc9578f420fc9bf6
rafael16phb <rafael16phb@gmail.com>
2024-02-17 17:10:35 -0300

    fix: correção da posição da tag script no html

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 const input = document.querySelector("input");
 const addButton = document.querySelector(".adicionar");
 const tarefasContainer = document.querySelector("#tarefas");
-const teste = document.getElementsByClassName(".teste");
 let tarefas = [];
 
 addButton.addEventListener("click", addTarefa);
 
 function addTarefa() {
   tarefas.push(input.value);
-  imprimirTarefa();
   input.value = "";
+  imprimirTarefa();
 }
 
 function imprimirTarefa() {
+  tarefasContainer.innerHTML = "";
   for (let index = 0; index < tarefas.length; index++) {
     const tarefa = tarefas[index];
 
     const textoTarefa = document.createElement("p");
     textoTarefa.innerText = tarefa;
-    teste.appendChild(textoTarefa);
+    tarefasContainer.appendChild(textoTarefa);
   }
 }
